6f5f7510129bfdac201166965f74132bb169a92c
BilleTheBug <dr.iversen@hotmail.com>
2017-02-27 10:54:36 +0100

    added untracked files

---
+++ src/app/menu/delete/delete-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bf-delete-view',
+  templateUrl: 'delete-view.component.html',
+  styleUrls: ['delete-view.component.css']
+})
+export class DeleteViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
